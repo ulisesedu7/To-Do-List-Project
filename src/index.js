506b34a4a0159da4bcc0f60354bc12c8de51b8a6
Ulises Largaespada <ulisesedu7@users.noreply.github.com>
2022-05-31 15:47:35 -0600

    Create HTML dynamically with JS

--- src/index.js
+++ src/index.js
@@ -3,27 +3,38 @@ import './style.css';
 
 // Main Function 
 const toDoListItems = [{
-  description: 'Finish the project of today',
   completed: false,
+  description: 'Finish the project of today',
   index: 0,
 },
 {
-  description: 'Finish additional reading exercises',
   completed: false,
+  description: 'Finish additional reading exercises',
   index: 1,
 },
 {
-  description: 'Finish webpack exercise',
   completed: true,
+  description: 'Finish webpack exercise',
   index: 2,
 }];
 
+/*
+Create HTML Dynamically
+*/
 const toDoListContainer = document.getElementById('to-do-list');
 
-function genToDoListMarkUp(description, completed, index) {
+function genToDoListMarkUp(completed, description, index) {
   const toDoListMarkup = `
-    <li></li>
-    <li></li>
-    <li></li>
-  `
+    <article class="list-items-c">
+      <input class="to-do-input" type="checkbox" id="${completed}">
+      <p class="to-do-des">${description}</p>
+      <i class="fa-solid fa-ellipsis-vertical" id="${index}"></i>
+    </article>
+  `;
+
+  return toDoListMarkup;
 }
+
+const toDoListMarkUp = toDoListItems.reduce((acc, {completed, description, index}) => `${acc}${genToDoListMarkUp(completed, description, index)}`, '');
+
+toDoListContainer.insertAdjacentHTML('afterbegin', toDoListMarkUp);
