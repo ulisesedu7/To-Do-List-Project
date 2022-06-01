112b39c235b8ebcefa92a8fa69b0a559cd0944f0
Ulises Largaespada <ulisesedu7@users.noreply.github.com>
2022-06-01 11:31:04 -0600

    Fix Linters issue 2

--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,23 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'production',
   entry: './src/index.js',
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-  ],
   devServer: {
     static: './dist',
     port: 3000,
     open: true,
     hot: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  mode: 'production',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
     clean: true,
   },
   module: {
